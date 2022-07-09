f5a1e1631c1c1682b61431d1c40a6bcfb189b3a4
atiQue <atiqurrahman7465@gmail.com>
2022-07-09 12:55:55 +0600

    [HP] - initial commit.

---
+++ src/main/java/com/example/springboothelperproject/SpringBootHelperProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothelperproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelperProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelperProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboothelperproject/SpringBootHelperProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothelperproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelperProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
